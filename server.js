7c6a49e08ed4826af1892c8738a0890a98192922
livea0624-boop <livea0624@gmail.com>
2026-09-15 04:19:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-const express=require('express');
-const path=require('path');
-const app=express();
-const PORT=process.env.PORT||3000;
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(express.static(__dirname));
-app.get('/api/status',(req,res)=>res.json({ok:true,app:'GARAGE cloud test'}));
+
+app.get('/', (req, res) => {
+  res.send('GARAGE cloud работает!');
+});
+
+app.get('/api/status', (req, res) => {
+  res.json({
+    ok: true,
+    app: 'GARAGE cloud test'
+  });
+});
+
 app.listen(PORT, '0.0.0.0', () => {
   console.log('GARAGE running ' + PORT);
 });
