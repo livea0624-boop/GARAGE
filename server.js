a5ca71e3f0ddc322caa560369fc7026b2b38d2c3
livea0624-boop <livea0624@gmail.com>
2026-09-15 03:57:18 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,8 @@
+const express=require('express');
+const path=require('path');
+const app=express();
+const PORT=process.env.PORT||3000;
+app.use(express.json());
+app.use(express.static(path.join(__dirname,'public')));
+app.get('/api/status',(req,res)=>res.json({ok:true,app:'GARAGE cloud test'}));
+app.listen(PORT,()=>console.log('GARAGE running '+PORT));
