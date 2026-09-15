2263328426289efe75b2160ff1574ea566241884
livea0624-boop <livea0624@gmail.com>
2026-09-15 04:15:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,4 +5,6 @@ const PORT=process.env.PORT||3000;
 app.use(express.json());
 app.use(express.static(__dirname));
 app.get('/api/status',(req,res)=>res.json({ok:true,app:'GARAGE cloud test'}));
-app.listen(PORT,()=>console.log('GARAGE running '+PORT));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log('GARAGE running ' + PORT);
+});
